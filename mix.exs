7a3ee5c7b574560a24bbabe55107914118c445e5
Powell Kinney <powell@vin.li>
2017-01-18 15:01:53 -0600

    Bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Envelope.Mixfile do
 
   def project do
     [app: :envelope,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.2",
      description: description,
      package: package,
